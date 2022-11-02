2a0a5fd7eac27f9486e6db58fb44d7a05f26c7f4
Akexey93 <77586321+Akexey93@users.noreply.github.com>
2022-11-02 17:49:26 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
+const stream = new fs.ReadStream(path.join(__dirname,'text.txt') , 'utf-8' )
+
+stream.on('readable', function(){
+    const data = stream.read()
+    if(data!== null){
+        console.log(data)
+    }
+})
+
+
